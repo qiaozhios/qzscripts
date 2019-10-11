32e12afa6f109d42867b2a7aeedd327c3a6fe3ef
qiaozhios <qiiaozhi@gmail.com>
2019-10-11 11:11:20 +0800

    feat: 给UNITE.py加上注释

--- bioinformatics/microbial_identification/gen_taxmap/UNITE.py
+++ bioinformatics/microbial_identification/gen_taxmap/UNITE.py
@@ -5,6 +5,7 @@ import click
 from Bio import SeqIO
 
 
+# 修改原始ID为PREFIX_数字形式
 PREFIX = 'UNITE'
 
 
@@ -18,10 +19,13 @@ def main(fasta, taxmap, raw_fasta):
         for r in SeqIO.parse(raw_fp, 'fasta'):
             cnt += 1
             raw_id = r.id
+            # 修改ID
             new_id = '%s_%d' % (PREFIX, cnt)
             r.id = new_id
+            # 清空其他属性
             r.name = ''
             r.description = ''
+            # 获取物种名称
             tax = raw_id.split('|')[-1]
             fp.write(r.format('fasta'))
             taxmap_fp.write('%s\t%s\t%s\n' % (new_id, tax, raw_id))
